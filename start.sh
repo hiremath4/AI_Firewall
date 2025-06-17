502fe16c6a5dc7f20a6ebda53d97cc6268047eb0
SUMIT HIREMATH <123145236+hiremath4@users.noreply.github.com>
2025-06-17 19:52:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run backend in background
-python manage.py runserver 0.0.0.0:$PORT &
+python manage.py runserver
 
 # Run frontend (e.g., Streamlit)
-streamlit run Home.py --server.port 10000 --server.enableCORS false
+streamlit run Home.py 
