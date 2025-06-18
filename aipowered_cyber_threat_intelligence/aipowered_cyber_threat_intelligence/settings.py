04d5b7636850364003d79df03811fbf5f3d5d150
SUMIT HIREMATH <123145236+hiremath4@users.noreply.github.com>
2025-06-18 21:49:20 +0530

    Update settings.py

--- aipowered_cyber_threat_intelligence/aipowered_cyber_threat_intelligence/settings.py
+++ aipowered_cyber_threat_intelligence/aipowered_cyber_threat_intelligence/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-myt#yi36g8q)2draq7*kugc)x#ioa2og%k68bc1($=k@)v2jld
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-backend-lltu.onrender.com','ai-firewall-1.onrender.com']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
